06b809f0257195b46b74dd73854a448998ad7943
Morten Kristensen <msk@nullpointer.dk>
2018-08-10 22:02:55 +0200

    Lookup designated channel ID

--- tourney.py
+++ tourney.py
@@ -4,6 +4,18 @@ from slackclient import SlackClient
 
 client = SlackClient(os.environ.get("TOURNEY_BOT_TOKEN"))
 bot_id = None
+all_channels = None
+
+CHANNEL_NAME = "foosball"
+
+def get_channels():
+  return client.api_call("channels.list", exclude_archived=1, exclude_members=1)["channels"]
+
+def lookup_channel_name(name):
+  for channel in all_channels:
+    if channel["name"] == name:
+      return channel["id"]
+  return None
 
 if __name__ == "__main__":
   if not client.rtm_connect(with_team_state=False):
@@ -14,3 +26,11 @@ if __name__ == "__main__":
 
   bot_id = client.api_call("auth.test")["user_id"]
   print("Tourney bot ID: {}".format(bot_id))
+
+  # Find the channel ID of designated channel name.
+  all_channels = get_channels()
+  channel_id = lookup_channel_name(CHANNEL_NAME)
+  if channel_id is None:
+    print("Could not find ID for channel: {}".format(CHANNEL_NAME))
+    exit(1)
+  print("#{} channel ID: {}".format(CHANNEL_NAME, channel_id))
