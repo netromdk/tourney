f6cff4f3cda9cd2e37ab06c0f1c94ae475f9ddba
Morten Kristensen <msk@nullpointer.dk>
2018-08-17 07:39:51 +0200

    Added :fire: as positive reaction

--- tourney/constants.py
+++ tourney/constants.py
@@ -11,5 +11,5 @@ MORNING_ANNOUNCE_HOUR = 9
 MIDDAY_ANNOUNCE_HOUR = 11
 MIDDAY_ANNOUNCE_MINUTE = 50
 
-POSITIVE_REACTIONS = ["+1", "the_horns", "metal", "raised_hands", "ok", "ok_hand"]
+POSITIVE_REACTIONS = ["+1", "the_horns", "metal", "raised_hands", "ok", "ok_hand", "fire"]
 NEGATIVE_REACTIONS = ["-1", "middle_finger"]
