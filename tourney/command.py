d7d10e0ee342ec2c320fa11467ce6a4b51630efd
Morten Kristensen <msk@nullpointer.dk>
2018-09-16 13:10:50 +0200

    Made Command class abstract with execute function

--- tourney/command.py
+++ tourney/command.py
@@ -1,31 +1,47 @@
 import re
-from .config import Config
-from .constants import COMMAND_REGEX, PRIVILEGED_COMMANDS
+from abc import ABC, abstractmethod
 
-class Command:
+from .config import Config
+from .constants import PRIVILEGED_COMMANDS
+
+class Command(ABC):
   """Command encapsulates a command issued by a user and with optional arguments."""
 
-  def __init__(self, user_id, command, args="", channel=None):
-    """`user_id` is the command issuer's ID, `command` is the issued command text, `args` is the
-optional arguments string, and `channel` is the optional channel the user issued the command
-from."""
-    self.user_id = user_id
-    self.command = command.strip().lower()
-    self.args = args
-    self.channel = channel
+  def __init__(self, name):
+    self.__name = name
+    self.__user_id = None
+    self.__args = ""
+    self.__channel = None
 
-  @staticmethod
-  def parse(event):
-    """Parse command from RTM event."""
-    msg = event["text"].strip()
-    m = re.match(COMMAND_REGEX, msg)
-    if not m:
-      return None
-    return Command(user_id=event["user"], command=m.group(1), args=m.group(2).strip(), \
-      channel=event["channel"])
+  def name(self):
+    """Command name."""
+    return self.__name
+
+  def user_id(self):
+    return self.__user_id
+
+  def set_user_id(self, user_id):
+    self.__user_id = user_id
+
+  def args(self):
+    return self.__args
+
+  def set_args(self, args):
+    self.__args = args
+
+  def channel(self):
+    return self.__channel
+
+  def set_channel(self, channel):
+    self.__channel = channel
 
   def allowed(self):
     """Check if user, who wrote command is allowed to execute it."""
-    if self.command in PRIVILEGED_COMMANDS:
-      return self.user_id in Config.get().privileged_users()
+    if self.name() in PRIVILEGED_COMMANDS:
+      return self.user_id() in Config.get().privileged_users()
     return True
+
+  @abstractmethod
+  def execute(self):
+    """Action to execute. Returns response to send or None on failure."""
+    pass
