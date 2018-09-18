8204f90d39f99bf0e118f29c80cfd6027a49a863
Morten Kristensen <msk@nullpointer.dk>
2018-09-18 19:51:26 +0200

    Removed unused symbol from tourney.commands

--- tourney/commands/__init__.py
+++ tourney/commands/__init__.py
@@ -1,4 +1,3 @@
-from . import command
 from .help_command import HelpCommand
 from .list_command import ListCommand
 from .join_command import JoinCommand
@@ -10,7 +9,6 @@ from .mystats_command import MyStatsCommand
 from .undoteams_command import UndoTeamsCommand
 
 __all__ = [
-  "command",
   "HelpCommand",
   "ListCommand",
   "JoinCommand",
