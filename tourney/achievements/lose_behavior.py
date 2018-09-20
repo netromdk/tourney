3016effb517e2d489a8ce8608d01d26849fa028f
Morten Kristensen <msk@nullpointer.dk>
2018-09-20 20:41:36 +0200

    Added winning and losing scores to win and lose behaviors

--- tourney/achievements/lose_behavior.py
+++ tourney/achievements/lose_behavior.py
@@ -1,9 +1,17 @@
 from .behavior import Behavior, LOSE_BEHAVIOR
 
 class LoseBehavior(Behavior):
-  def __init__(self, user_id, rounds):
+  def __init__(self, user_id, rounds, winner_score, loser_score):
     super(LoseBehavior, self).__init__(LOSE_BEHAVIOR, user_id)
     self.__rounds = rounds
+    self.__winner_score
+    self.__loser_score
 
   def rounds(self):
     return self.__rounds
+
+  def winner_score(self):
+    return self.__winner_score
+
+  def loser_score(self):
+    return self.__loser_score
--- tourney/achievements/win_behavior.py
+++ tourney/achievements/win_behavior.py
@@ -1,9 +1,17 @@
 from .behavior import Behavior, WIN_BEHAVIOR
 
 class WinBehavior(Behavior):
-  def __init__(self, user_id, rounds):
+  def __init__(self, user_id, rounds, winner_score, loser_score):
     super(WinBehavior, self).__init__(WIN_BEHAVIOR, user_id)
     self.__rounds = rounds
+    self.__winner_score
+    self.__loser_score
 
   def rounds(self):
     return self.__rounds
+
+  def winner_score(self):
+    return self.__winner_score
+
+  def loser_score(self):
+    return self.__loser_score
