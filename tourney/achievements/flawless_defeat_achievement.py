7446b2aeb3d76247809a62ddcb91d67b6473358b
Morten Kristensen <msk@nullpointer.dk>
2018-10-02 20:07:44 +0200

    Removed tier-related from non-tiered achievements

--- tourney/achievements/flawless_defeat_achievement.py
+++ tourney/achievements/flawless_defeat_achievement.py
@@ -25,15 +25,3 @@ class FlawlessDefeatAchievement(Achievement):
 
   def achieved(self, user_id):
     return user_id in self.data and self.data[user_id]
-
-  def progress(self, user_id):
-    return 0
-
-  def next_tier(self, user_id):
-    return None
-
-  def tiered_name(self, user_id):
-    return self.name()
-
-  def tiered_description(self, user_id):
-    return self.description()
