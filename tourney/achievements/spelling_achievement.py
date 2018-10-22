89c8c56dc27b00220930c9aa197e979d930a4768
Sean Tang Geggie <sean@luxion.com>
2018-10-22 10:41:26 +0200

    Updated new achievements to use new structure

--- tourney/achievements/spelling_achievement.py
+++ tourney/achievements/spelling_achievement.py
@@ -3,7 +3,7 @@ from .behavior import INVOKE_BEHAVIOR
 
 class SpellingAchievement(Achievement):
   def __init__(self):
-    super(RtfmAchievement, self).__init__("Spelling")
+    super(SpellingAchievement, self).__init__("Spelling")
 
   def name(self):
     return "I Before E"
@@ -25,15 +25,3 @@ class SpellingAchievement(Achievement):
 
   def achieved(self, user_id):
     return user_id in self.data and self.data[user_id]
-
-  def progress(self, user_id):
-    return 0
-
-  def next_tier(self, user_id):
-    return None
-
-  def tiered_name(self, user_id):
-    return self.name()
-
-  def tiered_description(self, user_id):
-    return self.description()
--- tourney/achievements/threeplayer_win_achievement.py
+++ tourney/achievements/threeplayer_win_achievement.py
@@ -1,20 +1,13 @@
-from .achievement import Achievement
+from .tiered_achievement import TieredAchievement
 from .behavior import WIN_BEHAVIOR
 
-TIERS = (
-  (1,   "Three Amigos", "Win a match in a three-player team."),
-  (3,   "Three Musketeers", "Win three matches in three-player teams."),
-)
-
-class ThreePlayerWinAchievement(Achievement):
+class ThreePlayerWinAchievement(TieredAchievement):
   def __init__(self):
+    tiers = (
+      (1,   "Three Amigos", "Win a match in a three-player team."),
+      (3,   "Three Musketeers", "Win three matches in three-player teams."),
+    )
     super(ThreePlayerWinAchievement, self).__init__("ThreePlayerWin")
-
-  def name(self):
-    return TIERS[0][1]
-
-  def description(self):
-    return TIERS[0][2]
 
   def accepted_behaviors(self):
     return [WIN_BEHAVIOR]
@@ -31,40 +24,3 @@ class ThreePlayerWinAchievement(Achievement):
         self.data[user_id][1] += 1
         return True
     return False
-
-  def achieved(self, user_id):
-    self.__check_init(user_id)
-    return self.data[user_id][1] >= 0
-
-  def progress(self, user_id):
-    self.__check_init(user_id)
-    return self.data[user_id][0]
-
-  def next_tier(self, user_id):
-    self.__check_init(user_id)
-    tier = self.data[user_id][1]
-    nt = tier+1
-    if nt >= len(TIERS):
-      return None
-    return TIERS[nt][0]
-
-  def tiered_name(self, user_id):
-    self.__check_init(user_id)
-    tier = self.data[user_id][1]
-    if tier == -1:
-      return self.name()
-    return TIERS[tier][1]
-
-  def tiered_description(self, user_id):
-    self.__check_init(user_id)
-    tier = self.data[user_id][1]
-    if tier == -1:
-      return self.description()
-    return TIERS[tier][2]
-
-  def __check_init(self, user_id):
-    if user_id not in self.data:
-      self.data[user_id] = [
-         0, # Won rounds amount
-        -1, # Tier
-      ]
