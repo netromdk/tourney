5fad9b7a1ca498c1e06301a27db1b6f35753fd27
Sean Tang Geggie <sean@luxion.com>
2018-10-26 08:21:48 +0200

    3v2 achievement made tiered.

--- tourney/achievements/threetwo_win_achievement.py
+++ tourney/achievements/threetwo_win_achievement.py
@@ -1,15 +1,14 @@
-from .achievement import Achievement
+from .tiered_achievement import TieredAchievement
 from .behavior import WIN_BEHAVIOR
 
-class ThreeVTwoWinAchievement(Achievement):
+class ThreeVTwoWinAchievement(TieredAchievement):
   def __init__(self):
-    super(ThreeVTwoWinAchievement, self).__init__("ThreeVTwoWin")
-
-  def name(self):
-    return "Strength in Numbers"
-
-  def description(self):
-    return "In a three-player team, beat a two player team"
+    tiers = (
+      (1,   "Strength in Numbers", "In a three-player team, beat a two player team."),
+      (10,  "E Pluribus", "In a three-player team, beat a two player team ten times."),
+      (50,  "My Name is Legion", "In a three-player team, beat a two player team fifty times."),
+    )
+    super(ThreeVTwoWinAchievement, self).__init__("ThreeVTwoWin", tiers)
 
   def accepted_behaviors(self):
     return [WIN_BEHAVIOR]
@@ -18,10 +17,13 @@ class ThreeVTwoWinAchievement(Achievement):
     user_id = behavior.user_id()
     user_team = behavior.winner_team()
     loser_team = behavior.loser_team()
+    self.__check_init(user_id)
     if len(user_team) == 3 and len(loser_team) == 2:
-      self.data[user_id] = True
-      return True
+      self.data[user_id][0] += 1
+      amount = self.data[user_id][0]
+      nt = self.next_tier(user_id)
+      if amount == nt or amount == nt+1:
+        self.data[user_id][1] += 1
+        return True
     return False
 
-  def achieved(self, user_id):
-    return user_id in self.data and self.data[user_id]
