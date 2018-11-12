23ee68ecc82a4a5188197d141b80bca727e6f576
Sean Tang Geggie <sean@luxion.com>
2018-11-12 10:22:16 +0100

    Travis fixes.

--- tourney/commands/stats_command.py
+++ tourney/commands/stats_command.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import calendar
 
 from tourney.stats import Stats
-from tourney.constants import STATS_DAYS_BACK
 
 class StatsCommand(Command):
   def __init__(self):
@@ -14,7 +13,8 @@ class StatsCommand(Command):
   def execute(self, lookup=None):
     stats = Stats.get()
 
-    this_month_filter = lambda x: datetime.fromtimestamp(x).month == datetime.today().month
+    def this_month_filter(time_stamp):
+      return datetime.fromtimestamp(time_stamp).month == datetime.today().month
 
     if not stats.generate(time_filter=this_month_filter):
       return "There are no recorded matches to generate statistics from!"
