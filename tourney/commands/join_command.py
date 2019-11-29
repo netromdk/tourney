21cc6e5d11b76f08ff98981becd5b76c4e675b8b
Sean Tang Geggie <sean@luxion.com>
2019-11-29 13:51:13 +0100

    Let players late-join 1p teams

--- tourney/commands/join_command.py
+++ tourney/commands/join_command.py
@@ -20,41 +20,47 @@ class JoinCommand(Command):
 
     created_teams = len(teams) > 0
     if created_teams:
-      joinable_teams = [x for x in teams if len(x) == 2]
+      if any([self.user_id() in t for t in teams]):
+        return "{}, you are _already_ on a team today!".format(user_name)
 
       scored_teams = []
       for score in Scores.get().today():
         scored_teams.append(score[1])
         scored_teams.append(score[3])
 
-      joinable_teams = [x for x in joinable_teams if x not in scored_teams]
+      # Late-join a 1p (preferred) or 2p team
+      for i in range(1, 3):
+        joinable_teams = [x for x in teams if len(x) <= i and x not in scored_teams]
 
-      if len(joinable_teams) > 0:
-        new_team = choice(joinable_teams)  # nosec
-        new_team_index = teams.index(new_team)
+        if len(joinable_teams) > 0:
+          new_team = choice(joinable_teams)  # nosec
+          new_team_index = teams.index(new_team)
 
-        new_team_name = team_names[new_team_index]
+          team_name = team_names[new_team_index]
 
-        new_team.append(self.user_id())
-        new_team_name = choice(TEAM_NAME_DECORATIONS)(new_team_name)  # nosec
+          new_team.append(self.user_id())
+          new_team_name = choice(TEAM_NAME_DECORATIONS)(team_name)  # nosec
 
-        teams[new_team_index] = new_team
-        team_names[new_team_index] = new_team_name
+          teams[new_team_index] = new_team
+          team_names[new_team_index] = new_team_name
 
-        state.set_teams(teams)
-        state.set_team_names(team_names)
-        state.save()
+          state.set_teams(teams)
+          state.set_team_names(team_names)
+          state.save()
 
-        fmt = ", ".join([lookup.user_name_by_id(uid) for uid in new_team])
-        formatted_team_name = "[T{}] *{}*: {}".format(new_team_index, new_team_name, fmt)
+          formatted_team_name = "[T{}] *{}*".format(new_team_index, team_name)
 
-        # This response must be made public because it changes the team for other players!
-        self.set_public(True)
+          fmt = ", ".join([lookup.user_name_by_id(uid) for uid in new_team])
+          formatted_new_team_name = "[T{}] *{}*: {}".format(new_team_index, new_team_name, fmt)
 
-        return "{}, you've joined existing team {}\nCheck `!schedule` for overview.".\
-          format(user_name, formatted_team_name)
-      else:
-        return "{}, you're too late. No late-joinable teams were found.".format(user_name)
+          # This response must be made public because it changes the team for other players!
+          self.set_public(True)
+
+          return "{}, you've joined existing team\n{}\nwhich becomes:\n{}\n"\
+            "Check `!schedule` for overview.".\
+            format(user_name, formatted_team_name, formatted_new_team_name)
+
+      return "{}, you're too late. No late-joinable teams were found.".format(user_name)
     else:
       if self.user_id() not in participants:
         state.add_participant(self.user_id())
