67f98989de3d5eed6eb965f001a32fb97526ab89
Sean Tang Geggie <sean@geggie.com>
2022-07-05 14:40:28 +0200

    Season parts may still be used off season

--- tourney/constants.py
+++ tourney/constants.py
@@ -1,8 +1,8 @@
 import sys
 from datetime import time, timedelta
 
-# Special seasons
-SEASONS = ["easter", "halloween", "xmas"]
+# Special seasons, with off-season mixin chance
+SEASONS = {"easter": 0.01, "halloween": 0.1, "xmas": 0.05}
 SEASON = "easter"
 
 # Will print all read events to stdout.
