2f5785be804feb3f454bc9c23b98f9fe71efda16
Sean Tang Geggie <sean@geggie.com>
2023-10-09 10:47:21 +0200

    First joiner achievement made tiered

--- tourney/achievements/first_joiner_achievement.py
+++ tourney/achievements/first_joiner_achievement.py
@@ -1,17 +1,23 @@
 from tourney.state import State
 
-from .achievement import Achievement
+from .tiered_achievement import TieredAchievement
 from .behavior import JOIN_BEHAVIOR
 
-class FirstJoinerAchievement(Achievement):
+class FirstJoinerAchievement(TieredAchievement):
   def __init__(self):
-    super().__init__("FirstJoiner")
+    tiers = (
+      (1,   "Early Bird",    "Be the first player to join the game of the day."),
+      (5,   "Earlier Bird",  "Be the first player to join five times."),
+      (10,  "Earliest Bird", "Be the first player to join ten times."),
+      (25,  "Night Owl",     "Be the first player to join twenty five times."),
+      (50,  "Pterosaur",     "Earliest of the birds! Be the first player to join fifty times."),
+    )
+    super().__init__("FirstJoiner", tiers)
 
-  def name(self):
-    return "Early Bird"
-
-  def description(self):
-    return "Be the first player to join the game of the day."
+  def achieved(self, user_id):
+    self.convert_from_untiered(user_id)
+    self.check_init(user_id)
+    return self.data[user_id][1] >= 0
 
   def accepted_behaviors(self):
     return [JOIN_BEHAVIOR]
@@ -20,12 +26,12 @@ class FirstJoinerAchievement(Achievement):
     user_id = behavior.user_id()
     state = State.get()
     participants = state.participants()
-    if user_id not in self.data:
-      self.data[user_id] = False
-    if len(participants) == 1 and not self.data[user_id]:
-      self.data[user_id] = True
-      return True
-    return False
 
-  def achieved(self, user_id):
-    return user_id in self.data and self.data[user_id]
+    if user_id in self.data:
+      self.convert_from_untiered(user_id)
+
+    if len(participants) == 1:
+      if self.inc_progress(user_id):
+        return True
+
+    return False
