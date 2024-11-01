680d09283a76d6508afec226e879d63d73c07fe4
seangeggie <sean@geggie.com>
2024-11-01 11:47:31 +0100

    Removed !start_season command

--- tourney/main.py
+++ tourney/main.py
@@ -178,8 +178,6 @@ def parse_command(activity: Activity):
     elif command == "speak" and len(args) > 0:
       cmd = SpeakCommand(client=client, text=args)
       channel = state.channel_id()
-    elif command == "startseason":
-      start_season()
 
   # Only send ephemeral message if command hasn't been parsed into a command instance because
   # `handle_command()` will do it otherwise.
